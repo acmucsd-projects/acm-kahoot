b00585c0dfeeca060321a9502fd4351b76c2aab0
jnarezo <21177895+jnarezo@users.noreply.github.com>
2020-08-05 15:18:54 -0700

    Add react-redux. Add file/folder boilerplate.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+
+import { store } from './store';
+import './styles/index.css';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
