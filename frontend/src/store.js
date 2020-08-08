55c32a64d5a051ff4a60a50cec0319e61e275b71
Joshua Narezo <21177895+jnarezo@users.noreply.github.com>
2020-08-07 18:02:24 -0700

    Change structure. Add manifeest.json. Add redux, redux-thunk.

--- frontend/src/store.js
+++
@@ -1,9 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-
-import reducer from './reducer';
-
-function getMiddleware() {
-  return applyMiddleware();
-}
-
-export const store = createStore(reducer, getMiddleware());
