5acb5e8f404f2f3309768e4a25653363b89c297c
thomas-gg <32282600+thomas-gg@users.noreply.github.com>
2020-08-11 22:20:22 -0700

    Updated to use postman
    Updated index to take postman instead of test html file

--- backend/index.js
+++ backend/index.js
@@ -17,34 +17,15 @@ mongoose.set('useCreateIndex', true);
 
 // name of the collection Pack(s)
 const Pack = mongoose.model("pack", packSchema);
-
-const Question = mongoose.model("question", questionSchema);
+//const Question = mongoose.model("question", questionSchema);
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
-app.post('/', function (req, res) {
-    console.log("packName " + req.body.packName);
-    var falseAnswers1 = [req.body.question1FAnswer1,req.body.question1FAnswer2];
-    var falseAnswers2 = [req.body.question2FAnswer1,req.body.question2FAnswer2];
-    const question1 = new Question({
-        id: 1,
-        name: req.body.question1Name,
-        question: req.body.question1Question,
-        answer: req.body.question1Answer,
-        falseAnswers: falseAnswers1
-    });
-    const question2 = new Question({
-        id: 2,
-        name: req.body.question2Name,
-        question: req.body.question2Question,
-        answer: req.body.question2Answer,
-        falseAnswers: falseAnswers2
-    });
-    var questions = [question1, question2];
+app.post('/packs', function (req, res) {
+    console.log("packName " + req.body.questions[0].name);
     const pack1 = new Pack({
-        id: 1,
-        name: req.body.packName,
-        questions: questions,
-        description: req.body.packDesc
+        name: req.body.name,
+        questions: req.body.questions,
+        description: req.body.description
     });
     pack1.save();
     res.redirect("/");
