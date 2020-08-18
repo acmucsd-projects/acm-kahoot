69866d3c86ca61e4a2ac1085dabd6bf544f13ffc
thomas-gg <32282600+thomas-gg@users.noreply.github.com>
2020-08-18 11:26:33 -0700

    Merged Routes and Socket creation

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 // Kahoot
-var {questionSchema,packSchema} = require("./models/questionSchema");
+var indexRouter = require('./routes/index');
 const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require('body-parser');
@@ -17,12 +17,26 @@ const port = 3000;
 
 const app = express();
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+require('dotenv').config();
+var name = process.env.NAME;
+var dbpswd = process.env.DB_PSW;
+var dbname = process.env.DB_NAME;
+var mongoURI = name == undefined ? "mongodb://localhost:27017/questionDB" : `mongodb+srv://${name}:${dbpswd}@cluster0-gzlxs.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology:true, useFindAndModify : false});
+
+//gets rid of deprecation warning
+mongoose.set('useCreateIndex', true);
 
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use('/', indexRouter);
 
 io.on('connection', socket => {
   socket.on('joinRoom', ({ username, room }) => {
@@ -50,53 +64,6 @@ io.on('connection', socket => {
     }
   });
 
-});
-
-
-
-
-
-// if questionDB doesn't exist create it
-mongoose.connect("mongodb://localhost:27017/questionDB", { useNewUrlParser : true , useUnifiedTopology : true});
-
-//gets rid of deprecation warning
-mongoose.set('useCreateIndex', true);
-
-// name of the collection Pack(s)
-const Pack = mongoose.model("pack", packSchema);
-
-const Question = mongoose.model("question", questionSchema);
-
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.post('/', function (req, res) {
-    console.log("packName " + req.body.packName);
-    var falseAnswers1 = [req.body.question1FAnswer1,req.body.question1FAnswer2];
-    var falseAnswers2 = [req.body.question2FAnswer1,req.body.question2FAnswer2];
-    const question1 = new Question({
-        id: 1,
-        name: req.body.question1Name,
-        question: req.body.question1Question,
-        answer: req.body.question1Answer,
-        falseAnswers: falseAnswers1
-    });
-    const question2 = new Question({
-        id: 2,
-        name: req.body.question2Name,
-        question: req.body.question2Question,
-        answer: req.body.question2Answer,
-        falseAnswers: falseAnswers2
-    });
-    var questions = [question1, question2];
-    const pack1 = new Pack({
-        id: 1,
-        name: req.body.packName,
-        questions: questions,
-        description: req.body.packDesc
-    });
-    pack1.save();
-    res.redirect("/");
 });
 
 server.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
