9f0135fa25ac6814164c39b07861e64f11d6dcfe
Joshua Narezo <21177895+jnarezo@users.noreply.github.com>
2020-08-23 10:44:12 -0700

    Implement Redux DevTools compatibility. (#21)
    Co-authored-by: Anh Pham <anh65498@gmail.com>

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -3,11 +3,13 @@ import thunk from 'redux-thunk';
 
 import reducer from './reducer';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 function getMiddleware() {
   return applyMiddleware(thunk);
 }
 
-export const store = createStore(
-  reducer,
-  getMiddleware()
-);
+export const store = createStore(reducer, composeEnhancers(
+    getMiddleware()
+));
+
