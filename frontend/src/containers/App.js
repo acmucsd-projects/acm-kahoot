da456178c7a5b46cec36ebbabb2309763a4e2385
Ishaan Harry Kavoori <ishaanharry@live.com>
2020-08-25 13:49:10 -0700

    committed App.js file

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import '../styles/App.css';
+import '../styles/App.scss';
+import LandingPage from './LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App glow">
+      <LandingPage/>
     </div>
   );
 }
