880805523ba402a74830d1e51ecd371b845684bc
prorick <56745348+prorick@users.noreply.github.com>
2020-08-30 00:50:05 -0700

    Universal Landing Page with routers! :)

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -2,10 +2,27 @@ import React from 'react';
 
 import '../styles/App.scss';
 
+import UniversalLandingPage from '../containers/UniversalLandingPage.js';
+
+import {
+  Route,
+  Switch
+} from "react-router-dom";
+
 function App() {
   return (
     <div className='App glow'>
-      
+      <Switch>
+        <Route path="/player">
+        </Route>
+
+        <Route path="/host">
+        </Route>
+        
+        <Route path="/">
+          <UniversalLandingPage/>
+        </Route>
+      </Switch>
     </div>
   );
 }
