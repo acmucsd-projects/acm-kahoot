6441af467176f153b365b1e0e5a12a6fbd1a88b2
Ishaan Harry Kavoori <ishaanharry@live.com>
2020-09-07 12:01:45 -0700

    fixed scss such that button no longer overlays text on smaller screens

--- frontend/src/containers/LandingPage.js
+++ frontend/src/containers/LandingPage.js
@@ -6,12 +6,14 @@ import BlobRight from '../assets/BlobRight.svg';
 import Button from '../components/Button';
 
 function LandingPage() {
-    return(
+    return( 
         <div>
             <img className={styles.blobleft} src={BlobLeft} alt="" />
             <img className={styles.blobright} src={BlobRight} alt="" />
-            <div className={styles.header}>ACM QUIZTIME</div>
-            <Button variant="landingpagebutton" label='PLAY' />
+            <div className={styles.container}>
+                <div className={styles.header}>ACM QUIZTIME</div>
+                <Button variant="landingpagebutton" label='PLAY' />
+            </div>
         </div>
     );
 }
