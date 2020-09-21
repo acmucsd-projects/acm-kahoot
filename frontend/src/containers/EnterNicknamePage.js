9c4bde7d3c3b8e0a4c615ad8f4391d670762599c
Ishaan Harry Kavoori <ishaanharry@live.com>
2020-09-21 15:39:38 -0700

    completed initial enter nickname page

--- frontend/src/containers/EnterNicknamePage.js
+++ frontend/src/containers/EnterNicknamePage.js
@@ -6,6 +6,8 @@ import Button from '../components/Button';
 function EnterNicknamePage(){
     return(
         <div>
+            <div className={styles.leftbox}>CODE: XXX-XXX</div>
+            <div className={styles.rightbox}>ACM QUIZTIME</div>
             <div className={styles.container}>
                 <div className={styles.header}>QUIZ NAME</div>
                 <input type="text" placeholder="ENTER NICKNAME" size="7=8"></input>
