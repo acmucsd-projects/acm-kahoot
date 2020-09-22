2a063df87b6557323287f4087e92ca58646d5b11
prorick <56745348+prorick@users.noreply.github.com>
2020-09-21 19:18:32 -0700

    Fixed the Waiting page so the color and shape corresponds with the button clicked by the player.

--- frontend/src/components/PlayerAnswerButton.js
+++ frontend/src/components/PlayerAnswerButton.js
@@ -6,6 +6,8 @@ import starImg from '../assets/star.svg';
 import squareImg from '../assets/square.svg';
 import circleImg from '../assets/circle.svg';
 
+import { Link } from 'react-router-dom';
+
 
 const shapeImages = {
     triangle: triangleImg,
@@ -15,23 +17,39 @@ const shapeImages = {
   };
 
 function PlayerAnswerButton(){
+
+    const setWaitingColor = (color) => {
+        localStorage.setItem("answerColor", color)
+        
+    }
+
     return (
         <div className={styles.AnswerGrid}> 
-            <button className={styles.answerButtonBlueStarflex } >
-                <img src={shapeImages.star} alt='star'  />
-            </button>
 
-            <button className={styles.answerButtonPinkCircleflex } >
-                <img src={shapeImages.circle} alt='circle'  />
-            </button>
+            <Link to="/answered">
+                <button className={styles.answerButtonBlueStarflex } onClick={() => setWaitingColor("blue")} >
+                    <img src={shapeImages.star} alt='star'  />
+                </button>
+            </Link>
 
-            <button className={styles.answerButtonGreenSquareflex } >
-                <img src={shapeImages.square} alt='square'  />
-            </button>
+            <Link to="/answered">
+                <button className={styles.answerButtonPinkCircleflex } onClick={() => setWaitingColor("pink")} >
+                    <img src={shapeImages.circle} alt='circle'  />
+                </button>
+            </Link>
 
-            <button className={styles.answerButtonOrangeTriangleflex } >
-                <img src={shapeImages.triangle} alt='triangle'  />
-            </button>
+            <Link to="/answered">
+                <button className={styles.answerButtonGreenSquareflex } onClick={() => setWaitingColor("green")} >
+                    <img src={shapeImages.square} alt='square'  />
+                </button>
+            </Link>
+
+            <Link to="/answered">
+                <button className={styles.answerButtonOrangeTriangleflex } onClick={() => setWaitingColor("orange")} >
+                    <img src={shapeImages.triangle} alt='triangle'  />
+                </button>
+
+            </Link>
        </div> 
 
     )
