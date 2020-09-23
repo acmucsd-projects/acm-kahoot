17dca3bb8efdd92331cb4ca75461069283e10875
Joshua Narezo <21177895+jnarezo@users.noreply.github.com>
2020-09-23 15:01:51 -0700

    Add socket.io events, start button.

--- frontend/src/containers/HostRoomView.js
+++ frontend/src/containers/HostRoomView.js
@@ -3,13 +3,14 @@ import socketIO from 'socket.io-client';
 
 import styles from '../styles/HostRoomView.module.scss';
 import PlayerList from '../components/PlayerList';
+import Button from '../components/Button';
 
 const ENDPOINT = 'http://localhost:3000';
 const INVALID_ROOM = 'already in-use';
 
 let socket;
 
-export default function HostRoomView() {
+export default function HostRoomView({ pack = {} }) {
   const [roomID, setRoomID] = useState(String(Math.floor(Math.random() * 100000)));
   const [users, setUsers] = useState([]);
 
@@ -26,34 +27,39 @@ export default function HostRoomView() {
       setRoomID(INVALID_ROOM);
     });
 
-    startRoom();
+    createRoom(roomID);
 
     return () => socket.disconnect();
   }, []);
 
-  function startRoom() {
-    if (!roomID) {
-      socket.emit('joinRoomAdmin', {
-        username: 'admin',
-        room: roomID,
-      });
-    }
-  }
-
-  let codeMessage;
-  if (roomID) {
-    codeMessage = (
-      <div className={styles.code}>
-        <span className={`${styles.accent} ${styles.large}`}>CODE:</span>
-        <span className={styles.large}>{roomID}</span>
-      </div>
-    );
-  }
+  const codeMessage = (
+    <div className={styles.code}>
+      <span className={`${styles.accent} ${styles.large}`}>CODE:</span>
+      <span className={styles.large}>{roomID}</span>
+    </div>
+  );
 
   return (
     <div className={styles.HostRoomView}>
       {codeMessage}
+      <button className={styles.start} onClick={startGame}>START</button>
       <PlayerList players={users} />
     </div>
   );
+}
+
+function createRoom(roomID) {
+  socket.emit('joinRoomAdmin', {
+    username: 'admin',
+    room: roomID,
+  });
+}
+
+function startGame(pack) {
+  socket.emit('start', pack);
+}
+
+function nextQuestion() {
+  socket.emit('nextQuestion');
+  socket.emit('startTime');
 }
