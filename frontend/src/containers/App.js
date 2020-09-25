f05baaaf1dd5c3fde42adfa14b2c3287edf4f60c
ishaanharry <ishaanharry@live.com>
2020-09-25 12:41:25 -0700

    Feature/fe player pages linked (#39)
    * Routers in App.js
    
    * Finish the UI and Client-Side Routers for `Universal Landing Page`, `Player Landing Page`, `Enter Code Page`, and `Enter Nickname Page`. 
    
    * Use URL param to pass the ROOM CODE between Enter Code page and Enter NickName page
    
    
    
    Co-authored-by: prorick <56745348+prorick@users.noreply.github.com>
    Co-authored-by: Anh <beepboop@Anhs-MacBook-Pro.local>
    Co-authored-by: Anh Pham <anh65498@gmail.com>

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,24 +1,22 @@
-import React from "react";
-
-import "../styles/App.scss";
-
+import React from 'react';
+import '../styles/App.scss';
+import PlayerLandingPage from './PlayerLandingPage';
 import UniversalLandingPage from "../containers/UniversalLandingPage.js";
-import EnterNicknamePage from "../containers/EnterNicknamePage.js";
 import GameCodePage from "../containers/GameCodePage.js";
-
+import EnterNicknamePage from "../containers/EnterNicknamePage.js";
 import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App glow">
       <Switch>
-        <Route path="/player" />
+        <Route path="/player" component={PlayerLandingPage} />
 
         <Route path="/host" />
 
-        <Route path="/enter-nickname" component={EnterNicknamePage}/>
-
         <Route path="/enter-code" component={GameCodePage}/>
+
+        <Route path="/enter-nickname/:code" component={EnterNicknamePage}/>
 
         <Route path="/" component={UniversalLandingPage} />
       </Switch>
--- frontend/src/containers/GameCodePage.js
+++ frontend/src/containers/GameCodePage.js
@@ -1,4 +1,5 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import '../styles/App.scss';
 import styles from '../styles/GameCodePage.module.scss';
 import BlobTopLeft from '../assets/Blob_TopLeft.svg';
@@ -6,8 +7,16 @@ import BlobTopRight from '../assets/Blob_TopRight.svg';
 import BlobBottomLeft from '../assets/Blob_BottomLeft.svg';
 import BlobBottomRight from '../assets/Blob_BottomRight.svg';
 import Button from '../components/Button';
+import { Link } from 'react-router-dom';
 
 function GameCodePage() {
+    const [code, setCode] = useState(0);
+
+    let handleChange = event => {
+        setCode(event.target.value);
+        //alert(code);
+    };
+
     return(
         <div>
             <img className={styles.blobtopleft} src={BlobTopLeft} alt="" />
@@ -16,21 +25,19 @@ function GameCodePage() {
             <img className={styles.blobbottomright} src={BlobBottomRight} alt="" />
             <div className={styles.container}>
                 <div className={styles.header}>ACM QUIZTIME</div>
-                {/* <div className={styles.header}>[insert the input form here]</div> */}
-                {/* <form>
-                    <label>
-                        <input type="text" />
-                    </label>
-                    <input type="submit" />
-                </form> */}
-                {/* <form>
-                    <label className={styles.input}>
-                        yeet somethin in here
-                        <input type="text" />
-                    </label>
-                </form> */}
-                <input type="text" placeholder="Game Code" size="7=8"></input>
-                <Button variant="gamecodepagebutton" label='ENTER' />
+                <input
+                    type="text"
+                    placeholder="Game Code"
+                    size="7=8"
+                    //onChange={() => setCode(event.target.value)}
+                    onChange={handleChange}>
+                </input>
+                <Link to={"/enter-nickname/" + code}>
+                    <Button
+                        variant={styles.gamecodepagebutton}
+                        label='ENTER'  
+                    />
+                </Link>
             </div>
         </div>
     );
