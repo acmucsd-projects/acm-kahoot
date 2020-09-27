7a8640f3cff9c27997f662277c86bf79f7ec1922
Joshua Narezo <21177895+jnarezo@users.noreply.github.com>
2020-09-27 15:53:13 -0700

    Add responsive styling.

--- frontend/src/components/UniversalLandingPageButton.js
+++ frontend/src/components/UniversalLandingPageButton.js
@@ -1,29 +1,27 @@
 import React from 'react';
-import styles from '../styles/UniversalLandingPageButton.module.scss';
 import { Link } from 'react-router-dom';
 
+import styles from '../styles/UniversalLandingPageButton.module.scss';
 
-
-function UniversalLandingPageButton(){
+export default function UniversalLandingPageButton() {
     return(
         <div>
-            <Link to="/player">
-                <button className={styles.playerButton } >
-                    <div className={styles.textStyle }>
-                        Player
-                    </div>            
-                </button>    
-            </Link>    
-            
-            <Link to="/host">
-                <button className={styles.hostButton } >
-                    <div className={styles.textStyle }>
-                        Host
-                    </div>                
-                </button>
-            </Link>
+            <div class={styles.container}>
+                <Link to="/player">
+                    <button className={styles.playerButton}>
+                        <div className={styles.textStyle}>
+                            Player
+                        </div>
+                    </button>
+                </Link>
+                <Link to="/host">
+                    <button className={styles.hostButton}>
+                        <div className={styles.textStyle}>
+                            Host
+                        </div>
+                    </button>
+                </Link>
+            </div>
         </div>
-    ) 
-
+    );
 }
-export default UniversalLandingPageButton;
